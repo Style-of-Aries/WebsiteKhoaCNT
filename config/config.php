8af93c7a75a4ee031fd34b875f6417bcbbdf1a35
Style-of-Aries <ductrong34end@gmail.com>
2026-01-24 16:20:58 +0700

    Reset main: remove all files

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('BASE_PATH', dirname(__DIR__)); // Trỏ về thư mục gốc
-define('VIEW_PATH', BASE_PATH . '/views/');
-define('PUBLIC_PATH', BASE_PATH . '/public/');
-define('BASE_URL', 'http://localhost/WebsiteKhoaCNT/public/');
